3009699dc7a3e7164e94ff3e70829ec44af2464d
omaru <omarux@gmail.com>
2019-07-16 12:29:14 -0600

    initial commit

---
+++ src/main/java/com/omaru/friendpackage/FriendPackageApplication.java
@@ -0,0 +1,13 @@
+package com.omaru.friendpackage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendPackageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendPackageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omaru/friendpackage/FriendPackageApplicationTests.java
@@ -0,0 +1,16 @@
+package com.omaru.friendpackage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FriendPackageApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
